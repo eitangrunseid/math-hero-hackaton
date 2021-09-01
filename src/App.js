77ea773a0b9fe9615f844a4fa31e0ba5f776de02
Eitan <eitangrunseid@gmail.com>
2021-09-01 13:10:35 +0300

    added context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "./pages/Main";
-import Game from "./pages/Game"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Game from "./pages/Game";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import "./App.css";
-
+import AppContext from "./context/AppContext.js";
 
 function App() {
+  const [userName, setUserName] = useState();
+  const [level, setLevel] = useState();
 	return (
-      <Router>
-		<div className="App">
-        <Switch>
-          <Route path="/game">
-            <Game />
-          </Route>
-          <Route path="/">
-            <Main />
-          </Route>
-        </Switch>
-    </div>
-    </Router>
+		<Router>
+			<AppContext.Provider
+				value={{
+					userName: userName,
+					setUserName: setUserName,
+					level: level,
+					setLevel: setLevel
+				}}
+			>
+				<div className="App">
+					<Switch>
+						<Route path="/game">
+							<Game />
+						</Route>
+						<Route path="/">
+							<Main />
+						</Route>
+					</Switch>
+				</div>
+			</AppContext.Provider>
+		</Router>
 	);
 }
 
