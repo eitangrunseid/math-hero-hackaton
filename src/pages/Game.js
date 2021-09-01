2beb33ce9350925a9da59f3b079d2d88b288d404
Eitan <eitangrunseid@gmail.com>
2021-09-01 14:08:56 +0300

    process bar

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Canvas from "../components/Canvas";
 import ExerciseGenerator from "../components/ExerciseGenerator";
+import ProcessBar from "../components/ProcessBar.jsx";
+import AppContext from "../context/AppContext";
 
 function Game() {
-  return (
-    <div>
-      <ExerciseGenerator />
-      <Canvas />
-    </div>
-  );
+	return (
+        <div>
+            <h1></h1>
+			<ProcessBar />
+			<ExerciseGenerator />
+			<Canvas />
+		</div>
+	);
 }
 
 export default Game;
