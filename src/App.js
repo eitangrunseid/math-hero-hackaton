83bfe925e28b0ce5c4e97e34ea6e1457915bafde
Eitan <eitangrunseid@gmail.com>
2021-09-01 10:42:59 +0300

    added painter and sending the draw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// import Painter from './components/Painter'
+import Canvas from './components/Canvas'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello All</h1>
+      {/* <Painter /> */}
+      <Canvas />
     </div>
   );
 }
