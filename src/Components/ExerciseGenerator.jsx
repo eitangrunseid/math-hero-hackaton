869ef175bbc188a5e3fb585207f997ebe2b5580a
ohadg888 <64354829+ohadg888@users.noreply.github.com>
2021-09-01 17:33:23 +0300

    fix components

---
+++ src/Components/ExerciseGenerator.jsx
@@ -0,0 +1,52 @@
+import React, { useContext, useState, useEffect } from "react";
+import "./ExerciseGenerator.css";
+import AppContext from "../context/AppContext";
+
+function ExerciseGenerator() {
+  const { setAnswer } = useContext(AppContext);
+  const [max, setMax] = useState(9);
+  const [output, setOutput] = useState([]);
+  const [instructions, setInstructions] = useState(
+    "How many Animals do you see?"
+  );
+  const [imojis, setImojis] = useState([
+    "🐒",
+    "🐈",
+    "🦨",
+    "🐿",
+    "🐇",
+    "🦔",
+    "🐫",
+  ]);
+
+  useEffect(() => {}, []);
+
+  useEffect(() => {
+    if (max !== 0) {
+      const initArray = [];
+      const randImoji = imojis[Math.floor(Math.random() * imojis.length)];
+      for (let i = 0; i < Math.floor(Math.random() * max + 1); i++) {
+        initArray.push(randImoji);
+      }
+      setAnswer(initArray.length);
+      setOutput(initArray);
+    }
+  }, [max]);
+
+  return (
+    <div className="generator-wrap">
+      <div className="instructions">{instructions}</div>
+      <div className="imojis">
+        {output.map((item, key) => {
+          return (
+            <div key={key} className="imoji-wrap">
+              {item}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ExerciseGenerator;
