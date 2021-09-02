9db82fbdfeee498393340e2aa036fedcebd822ec
Eitan <eitangrunseid@gmail.com>
2021-09-02 15:35:58 +0300

    final commit

--- src/Components/Alert.jsx
+++ src/Components/Alert.jsx
@@ -1,28 +1,40 @@
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
-export default function Alert() {
-  const MySwal = withReactContent(Swal);
+export default function Alert(str) {
+	const MySwal = withReactContent(Swal);
 
-  const myName = "Leo";
+	const status = str;
 
-  MySwal.fire({
-    didOpen: () => {
-      MySwal.clickConfirm();
-    },
-  }).then(() => {
-    if (myName === "Leo") {
-      MySwal.fire({
-        title: "Correct",
-        icon: "success",
-        text: "Nice job!",
-      });
-    } else {
-      MySwal.fire({
-        title: "Incorrect",
-        icon: "error",
-        text: "try again",
-      });
-    }
-  });
+	MySwal.fire({
+		didOpen: () => {
+			MySwal.clickConfirm();
+		}
+	}).then(() => {
+		if (status === "correct") {
+			MySwal.fire({
+				title: "Correct",
+				icon: "success",
+				text: "Nice job!"
+			});
+		} else if (status === "incorrect") {
+			MySwal.fire({
+				title: "Incorrect",
+				icon: "error",
+				text: "try again"
+			});
+		} else if (status === "next level") {
+			MySwal.fire({
+				title: "Great job! you got advanced to the next level",
+				icon: "success",
+				text: "Next Level"
+			});
+		} else {
+			MySwal.fire({
+				title: "Game Over",
+				icon: "error",
+				text: "Restart"
+			});
+		}
+	});
 }
--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -2,92 +2,91 @@ import React from "react";
 import axios from "axios";
 import AppContext from "../context/AppContext";
 import { ReactSketchCanvas } from "react-sketch-canvas";
-import "./Canvas.css";
 import Alert from "./Alert";
 import { Link } from "react-router-dom";
 
 const styles = {
-  border: "0.0625rem solid #9c9c9c",
-  borderRadius: "0.25rem",
+	border: "0.0625rem solid #9c9c9c",
+	borderRadius: "0.25rem"
 };
 
 const Canvas = class extends React.Component {
-  static contextType = AppContext;
-  constructor(props) {
-    super(props);
-    this.canvas = React.createRef();
-  }
+	static contextType = AppContext;
+	constructor(props) {
+		super(props);
+		this.canvas = React.createRef();
+	}
 
-  render() {
-    const context = this.context;
-    return (
-      <div className="painter-wrapper">
-        <ReactSketchCanvas
-          style={styles}
-          ref={this.canvas}
-          strokeWidth={20}
-          strokeColor="white"
-          canvasColor="black"
-          width="100%"
-          height="100%"
-        />
-        <button
-          onClick={() => {
-            this.canvas.current
-              .exportImage("png")
-              .then(async (data) => {
-                const image = {
-                  image: data,
-                };
+	render() {
+		const context = this.context;
+		return (
+			<div className="painter-wrapper">
+				<ReactSketchCanvas
+					style={styles}
+					ref={this.canvas}
+					strokeWidth={20}
+					strokeColor="white"
+					canvasColor="black"
+					width="100%"
+					height="100%"
+				/>
+				<button
+					onClick={() => {
+						this.canvas.current
+							.exportImage("png")
+							.then(async (data) => {
+								const image = {
+									image: data
+								};
 
-                axios({
-                  method: "post",
-                  url: "https://math-herro.herokuapp.com/api",
-                  headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json",
-                  },
-                  data: JSON.stringify(image),
-                }).then((response) => {
-                  if (!context.lives) {
-                    Alert("game over");
-                    <Link to="/" />;
-                    return;
-                  }
-                  if (context.progress === 100) {
-                    Alert("next level");
-                    return;
-                  }
-                  if (response.data[1].Num0 == context.answer) {
-                    context.setProgress((prev) => prev + 20);
-                    Alert("correct");
-                    context.setQuestion(!context.question);
-                    this.canvas.current.clearCanvas();
-                  } else {
-                    Alert("incorrect");
-                    context.setLives((prev) => prev - 1);
-                    context.setQuestion(!context.question);
-                    this.canvas.current.clearCanvas();
-                  }
-                });
-              })
-              .catch((e) => {
-                console.log(e);
-              });
-          }}
-        >
-          Get Image
-        </button>
-        <button
-          onClick={() => {
-            this.canvas.current.clearCanvas();
-          }}
-        >
-          Clear
-        </button>
-      </div>
-    );
-  }
+								axios({
+									method: "post",
+									url: "https://math-herro.herokuapp.com/api",
+									headers: {
+										"Access-Control-Allow-Origin": "*",
+										"Content-Type": "application/json"
+									},
+									data: JSON.stringify(image)
+								}).then((response) => {
+									if (!context.lives) {
+										Alert("game over");
+										<Link to="/" />;
+										return;
+									}
+									if (context.progress === 100) {
+										Alert("next level");
+										return;
+									}
+									if (response.data[1].Num0 == context.answer) {
+										context.setProgress((prev) => prev + 20);
+										Alert("correct");
+										context.setQuestion(!context.question);
+										this.canvas.current.clearCanvas();
+									} else {
+										Alert("incorrect");
+										context.setLives((prev) => prev - 1);
+										context.setQuestion(!context.question);
+										this.canvas.current.clearCanvas();
+									}
+								});
+							})
+							.catch((e) => {
+								console.log(e);
+							});
+					}}
+				>
+					Get Image
+				</button>
+				<button
+					onClick={() => {
+						this.canvas.current.clearCanvas();
+					}}
+				>
+					Clear
+				</button>
+			</div>
+		);
+	}
 };
 
 export default Canvas;
