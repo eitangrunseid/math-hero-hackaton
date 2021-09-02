5822417b4d1422b40621998397a0e7238b2af85f
Leonardo Janashvili <leojanashvili@gmail.com>
2021-09-02 11:43:41 +0300

    merged from main

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -8,15 +8,16 @@ export default function HomePage() {
 
   return (
     <div>
+      <loader />
       <h1 className="title">Math Hero</h1>
       <h2 className="your-name">Your Name</h2>
-      <div>
+      <div className="container">
         <input
           type="text"
           onKeyUp={(e) => {
             setName(e.target.value);
           }}
-          id="name"
+          id="name-input"
         />
       </div>
       <button
