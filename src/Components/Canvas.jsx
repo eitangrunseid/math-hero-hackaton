0de6f15753c1a1b42000c72b087ccb6a63293104
Leonardo Janashvili <leojanashvili@gmail.com>
2021-09-02 15:48:00 +0300

    commiting class changes

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -31,60 +31,62 @@ const Canvas = class extends React.Component {
           width="100%"
           height="100%"
         />
-        <button
-          onClick={() => {
-            this.canvas.current
-              .exportImage("png")
-              .then(async (data) => {
-                const image = {
-                  image: data,
-                };
+        <div className="buttons-container">
+          <button
+            onClick={() => {
+              this.canvas.current
+                .exportImage("png")
+                .then(async (data) => {
+                  const image = {
+                    image: data,
+                  };
 
-                axios({
-                  method: "post",
-                  url: "https://math-herro.herokuapp.com/api",
-                  headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json",
-                  },
-                  data: JSON.stringify(image),
-                }).then((response) => {
-                  if (!context.lives) {
-                    Alert("game over");
-                    <Link to="/" />;
-                    return;
-                  }
-                  if (context.progress === 100) {
-                    Alert("next level");
-                    return;
-                  }
-                  if (response.data[1].Num0 == context.answer) {
-                    context.setProgress((prev) => prev + 20);
-                    Alert("correct");
-                    context.setQuestion(!context.question);
-                    this.canvas.current.clearCanvas();
-                  } else {
-                    Alert("incorrect");
-                    context.setLives((prev) => prev - 1);
-                    context.setQuestion(!context.question);
-                    this.canvas.current.clearCanvas();
-                  }
+                  axios({
+                    method: "post",
+                    url: "https://math-herro.herokuapp.com/api",
+                    headers: {
+                      "Access-Control-Allow-Origin": "*",
+                      "Content-Type": "application/json",
+                    },
+                    data: JSON.stringify(image),
+                  }).then((response) => {
+                    if (!context.lives) {
+                      Alert("game over");
+                      <Link to="/" />;
+                      return;
+                    }
+                    if (context.progress === 100) {
+                      Alert("next level");
+                      return;
+                    }
+                    if (response.data[1].Num0 == context.answer) {
+                      context.setProgress((prev) => prev + 20);
+                      Alert("correct");
+                      context.setQuestion(!context.question);
+                      this.canvas.current.clearCanvas();
+                    } else {
+                      Alert("incorrect");
+                      context.setLives((prev) => prev - 1);
+                      context.setQuestion(!context.question);
+                      this.canvas.current.clearCanvas();
+                    }
+                  });
+                })
+                .catch((e) => {
+                  console.log(e);
                 });
-              })
-              .catch((e) => {
-                console.log(e);
-              });
-          }}
-        >
-          Get Image
-        </button>
-        <button
-          onClick={() => {
-            this.canvas.current.clearCanvas();
-          }}
-        >
-          Clear
-        </button>
+            }}
+          >
+            Get Image
+          </button>
+          <button
+            onClick={() => {
+              this.canvas.current.clearCanvas();
+            }}
+          >
+            Clear
+          </button>
+        </div>
       </div>
     );
   }
